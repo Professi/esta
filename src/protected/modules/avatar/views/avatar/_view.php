39adad2f4cc3cb4fe0a4084ac95c620eb1db14f5
Christian Ehringfeld <c.ehringfeld@t-online.de>
2013-03-04 16:51:53 +0000

    #Yii User Management #Bitte neues mysql schema nutzen

--- src/protected/modules/avatar/views/avatar/_view.php
+++
@@ -1,6 +0,0 @@
-<div class="view" style="float: left;margin-right: 10px;">
-<? echo CHtml::link($data->getAvatar(true), array(
-			'//avatar/avatar/editAvatar', 'id' => $data->id)); ?> 
-<h3> <? echo CHtml::link($data->username, array(
-			'//avatar/avatar/editAvatar', 'id' => $data->id)); ?> </h3>
-</div>
