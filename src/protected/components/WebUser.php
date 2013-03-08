9d666e85968623c94ad6281baa7dcb8285015986
Christian Ehringfeld <c.ehringfeld@t-online.de>
2013-03-08 21:52:22 +0000

    #WebUser hinzugefügt für Zugriffsüberprüfung mit Yii::app()->user->checkAccess($rollenId); ... im vorherigen Commit vergessen

---
+++ src/protected/components/WebUser.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * @author Christian Ehringfeld <c.ehringfeld@t-online.de>
+ */
+class WebUser extends CWebUser{
+    /**
+     * Overrides a Yii method that is used for roles in controllers (accessRules).
+     *
+     * @param string $operation Name of the operation required (here, a role).
+     * @param mixed $params (opt) Parameters for this operation, usually the object to access.
+     * @return bool Permission granted?
+     */
+    public function checkAccess($operation, $params=array())
+    {
+        if (empty($this->id)) {
+            return false;
+        }
+        
+        if($this->getState("role") == 0) {
+         return true;   //Admin darf immer alles
+        }
+        return ($operation === $this->getState('role'));
+}
+    
+    
+    
+}
+
+?>
