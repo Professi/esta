45fa46f82aa1229c224c122bbd09546b51b3d6a9
David Mock <d.u.mock@googlemail.com>
2013-03-14 12:40:42 +0000

    importTeacher.php desgined.

--- src/protected/views/user/importTeacher.php
+++ src/protected/views/user/importTeacher.php
@@ -5,17 +5,20 @@
 /* @var $form CActiveForm */
 ?>
 
-
-<?php
-
-$form=$this->beginWidget('CActiveForm', array( 'id'=>'csv-form', 'enableAjaxValidation'=>true, 'htmlOptions' => array('enctype' => 'multipart/form-data'), ));
+<div class="row">
+    <div class="five columns centered">
+        <fieldset>
+<?php   $form=$this->beginWidget('CActiveForm', array( 'id'=>'csv-form', 'enableAjaxValidation'=>true, 'htmlOptions' => array('enctype' => 'multipart/form-data'), ));
 ?>
-
-<?php echo $form->labelEx($model,'file'); ?> <?php echo $form->fileField($model,'file'); ?> <?php echo $form->error($model, 'file'); ?>
-
-
-
+<?php echo $form->fileField($model,'file'); ?> 
+<?php echo $form->error($model, 'file'); ?>
 <?php echo CHtml::submitButton('Importieren', array('class' => 'button')); ?>
 
 <?php $this->endWidget(); ?>
+        </fieldset>
+    </div>
+</div>
+
+
+
 
