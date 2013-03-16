2879e7e6fa656c2c063b8b9483e5b6d95c5cc3d5
Matthias Unterbusch <matthias.unterbusch@web.de>
2013-03-16 12:28:31 +0000

    #impressum.php

--- src/protected/views/site/pages/impressum.php
+++ src/protected/views/site/pages/impressum.php
@@ -7,14 +7,16 @@ $this->breadcrumbs=array(
 ?>
 <div class="row">
     <div class="twelve columns">
+            <p>Br&uuml;hlwiesenschule, Gartenstra&szlige 28, 65719 Hofheim </p>
+            <p>Die Elternsprechtagssoftware ESTA ist ein Schulprojekt der Br&uuml;hlwiesenschule. ESTA ist mit der GPL v3 lizensiert:</p>
+            <p>ESTA&nbsp;&nbsp;<a href="http://www.gnu.org/licenses/" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
             <p>Diese Seite wurde mit Hilfe der folgenden Ressourcen entwickelt und erstellt:</p>
             <p>ZURB Foundation Framework&nbsp;&nbsp;<a href="http://foundation.zurb.com"><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
             <p>IcoMoon  Icon Fonts&nbsp;&nbsp;<a href="http://icomoon.io" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
             <p>Google Web Fonts&nbsp;&nbsp;<a href="http://www.google.com/webfonts" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
             <p>Free Stock Textures&nbsp;&nbsp;<a href="http://freestocktextures.com" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
             <p>Yii Framework&nbsp;&nbsp;<a href="http://www.yiiframework.com" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
-            <p>Die Elternsprechtagssoftware ESTA ist ein Schulprojekt der Brühlwiesenschule. ESTA ist mit der GPL v3 lizensiert:</p>
-            <p>ESTA&nbsp;&nbsp;<a href="http://www.gnu.de/documents/gpl.de.html" ><span aria-hidden="true" data-icon="&#xe006;">&nbsp;Link</span></a></p>
+            <p>Copyright (C) 2013  Christian Ehringfeld, David Mock, Matthias Unterbusch</p>
             <p class="text-center"><?php echo CHtml::link('<b>ZurÃ¼ck zur Startseite</b>', 'index.php'); ?> </p>
     </div>
 </div>
