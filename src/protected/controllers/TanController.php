66a5a1a3267d5c40364d35f3890a081c0706d9f8
Christian Ehringfeld <c.ehringfeld@t-online.de>
2013-03-21 07:46:39 +0000

    #TanController

--- src/protected/controllers/TanController.php
+++ src/protected/controllers/TanController.php
@@ -48,11 +48,7 @@ class TanController extends Controller {
         return array(
             array('allow',
                 'actions' => array('genTans'),
-                'roles' => array('2'),
-            ),
-            array('allow',
-                'actions' => array('admin', 'genTans'),
-                'roles' => array('1'),
+                'roles' => array('2','1'),
             ),
             array('deny', // deny all users
                 'users' => array('*'),
@@ -61,25 +57,11 @@ class TanController extends Controller {
     }
 
     /**
-     * Displays a particular model.
-     * @param integer $id the ID of the model to be displayed
-     */
-    public function actionView($id) {
-        $this->render('view', array(
-            'model' => $this->loadModel($id),
-        ));
-    }
-
-    /**
      * Creates a new model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      */
     public function actionCreate() {
         $model = new Tan;
-
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
-
         if (isset($_POST['Tan'])) {
             $model->attributes = $_POST['Tan'];
             if ($model->save())
@@ -114,61 +96,6 @@ class TanController extends Controller {
     }
 
     /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
-    public function actionUpdate($id) {
-        $model = $this->loadModel($id);
-        if (isset($_POST['Tan'])) {
-            $model->attributes = $_POST['Tan'];
-            if ($model->save())
-                $this->redirect(array('view', 'id' => $model->tan));
-        }
-
-        $this->render('update', array(
-            'model' => $model,
-        ));
-    }
-
-    /**
-     * Deletes a particular model.
-     * If deletion is successful, the browser will be redirected to the 'admin' page.
-     * @param integer $id the ID of the model to be deleted
-     */
-    public function actionDelete($id) {
-        $this->loadModel($id)->delete();
-
-        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if (!isset($_GET['ajax']))
-            $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-    }
-
-    /**
-     * Lists all models.
-     */
-    public function actionIndex() {
-        $dataProvider = new CActiveDataProvider('Tan');
-        $this->render('index', array(
-            'dataProvider' => $dataProvider,
-        ));
-    }
-
-    /**
-     * Manages all models.
-     */
-    public function actionAdmin() {
-        $model = new Tan('search');
-        $model->unsetAttributes();  // clear any default values
-        if (isset($_GET['Tan']))
-            $model->attributes = $_GET['Tan'];
-
-        $this->render('admin', array(
-            'model' => $model,
-        ));
-    }
-
-    /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
      * @param integer $id the ID of the model to be loaded
@@ -182,16 +109,6 @@ class TanController extends Controller {
         return $model;
     }
 
-    /**
-     * Performs the AJAX validation.
-     * @param Tan $model the model to be validated
-     */
-    protected function performAjaxValidation($model) {
-        if (isset($_POST['ajax']) && $_POST['ajax'] === 'tan-form') {
-            echo CActiveForm::validate($model);
-            Yii::app()->end();
-        }
-    }
 /**
  * Generiert n-Tans
  * @param integer $count Anzahl der zu generierenden TAN's
