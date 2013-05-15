f6fb154df54a8046589fe08e09dd83bba24e0e03
Matthias Unterbusch <matthias.unterbusch@web.de>
2013-05-15 14:53:24 +0000

    #Update Praesentation/Elternsprechtagsanwendung(ESTA)_pre_alpha_0.0.0.1.ppt

--- docs/Grafiken/Praesentation/Realisierung (Ziele).mm
+++ docs/Grafiken/Praesentation/Realisierung (Ziele).mm
@@ -1,6 +1,6 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#0033ff" CREATED="1368542790464" ID="ID_1867870570" MODIFIED="1368544731924">
+<node COLOR="#0033ff" CREATED="1368542790464" ID="ID_1867870570" MODIFIED="1368607461330">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -10,10 +10,9 @@
       <font size="6">Realisierung (Ziele)</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#0033ff" WIDTH="4"/>
-<node COLOR="#00b439" CREATED="1368542868325" ID="ID_1850041085" MODIFIED="1368545436594" POSITION="right">
+<node COLOR="#00b439" CREATED="1368542868325" ID="ID_1850041085" MODIFIED="1368608421670" POSITION="right" VSHIFT="-14">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -23,10 +22,9 @@
       <font size="5">Barrierefreiheit</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
-<node COLOR="#990000" CREATED="1368544378850" ID="ID_969072851" MODIFIED="1368545436594" VSHIFT="-7">
+<node COLOR="#990000" CREATED="1368544378850" ID="ID_969072851" MODIFIED="1368607545671" VSHIFT="-7">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -39,15 +37,14 @@
       als Erg&#228;nzung
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
-<node COLOR="#990000" CREATED="1368544446169" ID="ID_1036951395" MODIFIED="1368545436594" TEXT="Starke Kontraste">
+<node COLOR="#990000" CREATED="1368544446169" ID="ID_1036951395" MODIFIED="1368607545671" TEXT="Starke Kontraste">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1368543111447" ID="ID_1008786131" MODIFIED="1368545436594" POSITION="left" VSHIFT="-11">
+<node COLOR="#00b439" CREATED="1368543111447" HGAP="26" ID="ID_1008786131" MODIFIED="1368608427332" POSITION="left" VSHIFT="-25">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -57,18 +54,17 @@
       <font size="5">Benutzerfreundlichkeit</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <arrowlink DESTINATION="ID_1008786131" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1981276619" STARTARROW="None" STARTINCLINATION="0;0;"/>
-<node COLOR="#990000" CREATED="1368545378402" ID="ID_1666698513" MODIFIED="1368545436594" TEXT="Einfaches Men&#xfc;">
+<node COLOR="#990000" CREATED="1368545378402" ID="ID_1666698513" MODIFIED="1368607545671" TEXT="Einfaches Men&#xfc;">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
-<node COLOR="#990000" CREATED="1368545393736" ID="ID_601705208" MODIFIED="1368545436594" TEXT="Viele Kurzanleitungen">
+<node COLOR="#990000" CREATED="1368545393736" ID="ID_601705208" MODIFIED="1368607545671" TEXT="Viele Kurzanleitungen">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1368544565267" HGAP="23" ID="ID_1019230123" MODIFIED="1368545436594" POSITION="right" VSHIFT="10">
+<node COLOR="#00b439" CREATED="1368544565267" HGAP="23" ID="ID_1019230123" MODIFIED="1368607545671" POSITION="right" VSHIFT="10">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -78,11 +74,10 @@
       <font size="5">Sicherheit</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <arrowlink DESTINATION="ID_1019230123" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1972835388" STARTARROW="None" STARTINCLINATION="0;0;"/>
-<node COLOR="#990000" CREATED="1368544605732" ID="ID_299960578" MODIFIED="1368545436594">
+<node COLOR="#990000" CREATED="1368544605732" ID="ID_299960578" MODIFIED="1368607545671">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -92,15 +87,14 @@
       Keine SQL-Incentions m&#246;glich
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
-<node COLOR="#990000" CREATED="1368544637485" ID="ID_1540721660" MODIFIED="1368545436594" TEXT="Kein Cross Site Scripting m&#xf6;glich">
+<node COLOR="#990000" CREATED="1368544637485" ID="ID_1540721660" MODIFIED="1368607545671" TEXT="Kein Cross Site Scripting m&#xf6;glich">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1368543173383" ID="ID_562573080" MODIFIED="1368545436594" POSITION="right" VSHIFT="7">
+<node COLOR="#00b439" CREATED="1368543173383" HGAP="23" ID="ID_562573080" MODIFIED="1368608423120" POSITION="right" VSHIFT="21">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -110,14 +104,13 @@
       <font size="5">Datenschutz</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
-<node COLOR="#990000" CREATED="1368544678933" ID="ID_563129468" MODIFIED="1368545436594" TEXT="Passwortverschl&#xfc;sselung">
+<node COLOR="#990000" CREATED="1368544678933" ID="ID_563129468" MODIFIED="1368607545671" TEXT="Passwortverschl&#xfc;sselung">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1368543322105" ID="ID_1950464932" MODIFIED="1368545436594" POSITION="left">
+<node COLOR="#00b439" CREATED="1368543322105" HGAP="34" ID="ID_1950464932" MODIFIED="1368608434009" POSITION="left">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -127,17 +120,16 @@
       <font size="5">Fehlerfreie Software</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
-<node COLOR="#990000" CREATED="1368545210466" ID="ID_1043455915" MODIFIED="1368545436594" TEXT="Ausf&#xfc;hrliches Testen">
+<node COLOR="#990000" CREATED="1368545210466" ID="ID_1043455915" MODIFIED="1368607545671" TEXT="Ausf&#xfc;hrliches Testen">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
-<node COLOR="#990000" CREATED="1368545221135" ID="ID_838074226" MODIFIED="1368545436594" TEXT="Unabh&#xe4;ngige Tester">
+<node COLOR="#990000" CREATED="1368545221135" ID="ID_838074226" MODIFIED="1368607545671" TEXT="Unabh&#xe4;ngige Tester">
 <edge COLOR="#990000" WIDTH="1"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1368543330607" ID="ID_624250770" MODIFIED="1368545442163" POSITION="left" VSHIFT="14">
+<node COLOR="#00b439" CREATED="1368543330607" ID="ID_624250770" MODIFIED="1368608424743" POSITION="left" VSHIFT="28">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -147,9 +139,17 @@
       <font size="5">Plattforumunabh&#228;ngigkeit</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1368607479801" ID="ID_261308755" MODIFIED="1368607545687" TEXT="Funktioniert auf allen Medien">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1368607490502" ID="ID_1349996459" MODIFIED="1368607545687" TEXT="Funktioniert mit allen Browsern">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1368607508473" ID="ID_1807044905" MODIFIED="1368607545687" TEXT="Funktioniert auf allen Plattformen">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
 </node>
 </node>
 </map>
