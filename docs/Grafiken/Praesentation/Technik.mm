97fd45df09d1656e7158a75f1438f1294cb623cc
Matthias Unterbusch <matthias.unterbusch@web.de>
2013-05-23 13:00:07 +0000

    #Update Praesentation/Elternsprechtagsanwendung(ESTA).ppt mit Technik.png

--- docs/Grafiken/Praesentation/Technik.mm
+++ docs/Grafiken/Praesentation/Technik.mm
@@ -10,8 +10,7 @@
       <font size="6">Technik</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#0033ff" WIDTH="4"/>
 <font NAME="SansSerif" SIZE="12"/>
 <node COLOR="#00b439" CREATED="1369306652110" HGAP="17" ID="ID_187390548" MODIFIED="1369307779021" POSITION="right" VSHIFT="-5">
@@ -24,12 +23,11 @@
       <font color="#000000" size="5">MVC</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306221530" HGAP="17" ID="ID_1454344363" MODIFIED="1369307790393" POSITION="right" VSHIFT="-10">
+<node COLOR="#00b439" CREATED="1369306221530" HGAP="17" ID="ID_1454344363" MODIFIED="1369312530081" POSITION="right" VSHIFT="-7">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -39,12 +37,11 @@
       <font color="#000000" size="5">PHP 5</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306666851" HGAP="19" ID="ID_1578141463" MODIFIED="1369307635958" POSITION="left" VSHIFT="-10">
+<node COLOR="#00b439" CREATED="1369306666851" HGAP="36" ID="ID_1578141463" MODIFIED="1369312505373" POSITION="left">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -54,12 +51,11 @@
       <font color="#000000" size="5">OOP</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306229727" HGAP="16" ID="ID_1412937547" MODIFIED="1369307654006" POSITION="left" VSHIFT="-7">
+<node COLOR="#00b439" CREATED="1369306229727" HGAP="30" ID="ID_1412937547" MODIFIED="1369312504047" POSITION="left" VSHIFT="4">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -69,12 +65,11 @@
       <font color="#000000" size="5">Yii Framework</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306241263" HGAP="21" ID="ID_90909791" MODIFIED="1369307803886" POSITION="right" VSHIFT="-12">
+<node COLOR="#00b439" CREATED="1369306241263" HGAP="21" ID="ID_90909791" MODIFIED="1369312563992" POSITION="right" VSHIFT="-14">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -84,19 +79,21 @@
       <font color="#000000" size="5">HTML</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306248774" HGAP="18" ID="ID_1822212796" MODIFIED="1369307712113" POSITION="left" VSHIFT="-8">
+<node COLOR="#00b439" CREATED="1369306248774" HGAP="25" ID="ID_1822212796" MODIFIED="1369312500054" POSITION="left" VSHIFT="-4">
 <richcontent TYPE="NODE"><html>
   <head>
     
   </head>
   <body>
-    <p>
-      <font color="#000000" size="5">ZURB Foundation Framework</font>
+    <p style="text-align: center">
+      <font color="#000000" size="5">ZURB Foundation </font>
+    </p>
+    <p style="text-align: center">
+      <font color="#000000" size="5">Framework</font>
     </p>
   </body>
 </html>
@@ -104,7 +101,7 @@
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306286032" HGAP="19" ID="ID_523346275" MODIFIED="1369307815648" POSITION="right" VSHIFT="5">
+<node COLOR="#00b439" CREATED="1369306286032" HGAP="19" ID="ID_523346275" MODIFIED="1369312554414" POSITION="right" VSHIFT="3">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -114,12 +111,11 @@
       <font color="#000000" size="5">Javascript</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306292935" ID="ID_252590189" MODIFIED="1369307747028" POSITION="left" VSHIFT="2">
+<node COLOR="#00b439" CREATED="1369306292935" ID="ID_252590189" MODIFIED="1369312492458" POSITION="left" VSHIFT="-5">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -129,12 +125,11 @@
       <font color="#000000" size="5">IcoMoon Icon Fonts</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306310491" ID="ID_401908544" MODIFIED="1369307825959" POSITION="right" VSHIFT="7">
+<node COLOR="#00b439" CREATED="1369306310491" HGAP="19" ID="ID_401908544" MODIFIED="1369312527117" POSITION="right">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -144,12 +139,11 @@
       <font color="#000000" size="5">Ajax</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306314726" HGAP="18" ID="ID_1546960258" MODIFIED="1369307756933" POSITION="left" VSHIFT="5">
+<node COLOR="#00b439" CREATED="1369306314726" HGAP="18" ID="ID_1546960258" MODIFIED="1369312494564" POSITION="left" VSHIFT="-2">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -159,12 +153,11 @@
       <font color="#000000" size="5">Google Webs Fonts</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306557728" ID="ID_1586463543" MODIFIED="1369307834975" POSITION="right" VSHIFT="7">
+<node COLOR="#00b439" CREATED="1369306557728" HGAP="21" ID="ID_1586463543" MODIFIED="1369312550796" POSITION="right" VSHIFT="6">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -174,12 +167,11 @@
       <font color="#000000" size="5">MySQL</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node COLOR="#00b439" CREATED="1369306581727" HGAP="13" ID="ID_1092410006" MODIFIED="1369307765809" POSITION="left" VSHIFT="7">
+<node COLOR="#00b439" CREATED="1369306581727" HGAP="16" ID="ID_1092410006" MODIFIED="1369312495952" POSITION="left" VSHIFT="-4">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -189,8 +181,7 @@
       <font color="#000000" size="5">Free Stock Textures</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#00b439" WIDTH="2"/>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
