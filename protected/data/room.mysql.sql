fb2e13df7b2286e8e9d047a618ee489489ded8ba
Christian Ehringfeld <c.ehringfeld@t-online.de>
2015-04-03 20:45:30 +0200

    db fix

--- protected/data/room.mysql.sql
+++ protected/data/room.mysql.sql
@@ -6,19 +6,24 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `room` (
 `id` int(11) NOT NULL PRIMARY KEY,
   `name` varchar(255) NOT NULL UNIQUE KEY
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
 CREATE TABLE IF NOT EXISTS `user_has_room` (
 `id` int(11) NOT NULL PRIMARY KEY,
   `user_id` int(11) NOT NULL,
   `room_id` int(11) NOT NULL,
 `date_id` int(11) NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+ALTER TABLE `room`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `user_has_room`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `user_has_room`
  ADD UNIQUE KEY `idx_user_has_room1` (`user_id`,`room_id`,`date_id`);
-
 
 ALTER TABLE `user_has_room`
 ADD CONSTRAINT `user_has_room_fk2` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
