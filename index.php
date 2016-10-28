7b531a8e39a3f5749b9bb1affcf0c61c64f50dcf
Christian Ehringfeld <c.ehringfeld@t-online.de>
2016-10-29 00:01:14 +0200

    ...

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ require_once dirname(__FILE__) . '/protected/components/globals.php';
 $yii = dirname(__FILE__) . '/framework/yii.php';
 $config = dirname(__FILE__) . '/protected/config/main.php';
 
-//define('YII_DEBUG', true);
+define('YII_DEBUG', false);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
